87630bec79a5e50ed5a52e54840a1d376fc4394c
Richard Robinson <rrobinson.campos@gmail.com>
2021-05-14 09:07:35 -0300

    description page receiving props from home

--- src/pages/DescriptionPage/index.jsx
+++ src/pages/DescriptionPage/index.jsx
@@ -1,9 +1,15 @@
+import { useLocation } from 'react-router';
 import { Container } from './styles';
 
 function DescriptionPage() {
+  let repoData = useLocation();
+
   return (
     <Container>
-      <h1>DescriptionPage</h1>
+      <h1>{repoData.state.name}</h1>
+      <h1>{repoData.state.description}</h1>
+      <h1>{repoData.state.owner}</h1>
+      <h1>{repoData.state.language}</h1>
     </Container>
   );
 }
