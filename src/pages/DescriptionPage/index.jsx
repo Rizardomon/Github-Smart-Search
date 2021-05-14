5decd7be2da907338a39bfa474413eeedac71819
Richard Robinson <rrobinson.campos@gmail.com>
2021-05-14 09:06:50 -0300

    Adding routes to description page

---
+++ src/pages/DescriptionPage/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function DescriptionPage() {
+  return (
+    <Container>
+      <h1>DescriptionPage</h1>
+    </Container>
+  );
+}
+
+export default DescriptionPage;
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import DescriptionPage from './pages/DescriptionPage';
 import HomePage from './pages/HomePage';
 
 function Routes() {
@@ -7,6 +8,7 @@ function Routes() {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact={true} component={HomePage} />
+        <Route path="/description" component={DescriptionPage} />
       </Switch>
     </BrowserRouter>
   );
